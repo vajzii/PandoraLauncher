7ec5ff7602c9d61d68980d867b29b9f7b778066c
Moulberry <james.jenour@protonmail.com>
2026-01-01 02:29:33 +0800

    Fix builds

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -6,4 +6,6 @@ cargo build --release --target x86_64-apple-darwin
 strip target/aarch64-apple-darwin/release/pandora_launcher
 strip target/x86_64-apple-darwin/release/pandora_launcher
 
+mkdir -p dist
+
 lipo -create -output dist/PandoraLauncher-macOS-Universal target/x86_64-apple-darwin/release/pandora_launcher target/aarch64-apple-darwin/release/pandora_launcher
--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -2,4 +2,7 @@ set -e
 
 cargo build --release --target x86_64-pc-windows-msvc
 strip target/x86_64-pc-windows-msvc/release/pandora_launcher.exe
+
+mkdir -p dist
+
 mv target/x86_64-pc-windows-msvc/release/pandora_launcher dist/PandoraLauncher-Windows-x86_64.exe
